c1000e042b12be1a6761036a5d223314d2775d11
Federico Erceg <federicohernanaerceg@gmail.com>
2023-08-09 17:10:29 -0300

    mod

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 async function obtenerProductos() {
-    const llamadoProductos = await fetch('./data.json');
+    const llamadoProductos = await fetch('../data.json');
     const data = await llamadoProductos.json();
     console.log(data);
 
