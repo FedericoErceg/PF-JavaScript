5d86a7a4861bc021a4c477361b616e7f8fb72405
Federico Erceg <federicohernanaerceg@gmail.com>
2023-08-10 00:05:40 -0300

    mod

--- js/main.js
+++ js/main.js
@@ -3,6 +3,77 @@ async function obtenerProductos() {
   const data = await llamadoProductos.json();
   const mainElement = document.querySelector("main");
 
+  let barraBusqueda = document.getElementById("barraDeBusqueda");
+  let menuDesplegable = document.getElementById("menuDesplegable");
+
+
+  function buscadorInput() {
+    let buscador = barraBusqueda.value.toLowerCase();
+    let resultadoBusqueda = data.filter((prod) => {
+      return prod.nombre.toLowerCase().includes(buscador);
+    });
+
+    menuDesplegable.innerHTML = "";
+    resultadoBusqueda.forEach((prod) => {
+      const li = document.createElement("li");
+      li.textContent = prod.nombre;
+      menuDesplegable.style.color = "black";
+      menuDesplegable.appendChild(li);
+    });
+
+    menuDesplegable.style.display =
+      resultadoBusqueda.length > 0 && buscador !== "" ? "block" : "none";
+
+    document.addEventListener("click", (event) => {
+      if (
+        !menuDesplegable.contains(event.target) &&
+        event.target !== barraBusqueda
+      ) {
+        menuDesplegable.style.display = "none";
+      }
+    });
+  }
+  barraBusqueda.addEventListener("input", buscadorInput);
+
+
+  menuDesplegable.addEventListener("click", (event) => {
+    const nombreProductoSeleccionado = event.target.textContent;
+    const productoSeleccionado = data.find(
+      (producto) => producto.nombre === nombreProductoSeleccionado
+    );
+
+    if (productoSeleccionado) {
+      const productoEnCarrito =
+        JSON.parse(localStorage.getItem("productoEnCarrito")) || [];
+      const productoExistente = productoEnCarrito.find(
+        (producto) => producto.id === productoSeleccionado.id
+      );
+
+      if (!productoExistente) {
+        productoEnCarrito.push({ ...productoSeleccionado, cantidad: 1 });
+      } else {
+        productoExistente.cantidad++;
+      }
+
+      localStorage.setItem(
+        "productoEnCarrito",
+        JSON.stringify(productoEnCarrito)
+      );
+
+      Toastify({
+        text: "Producto agregado al Carrito",
+        className: "info",
+        gravity: "bottom",
+        position: "right",
+        duration: 1500,
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+      }).showToast();
+    }
+  });
+
+
   const container = document.createElement("div");
   container.classList.add("row");
 
@@ -39,8 +110,8 @@ async function obtenerProductos() {
       );
 
       if (!productoExistente) {
-        productoEnCarrito.push({...e, cantidad: 1});
-      } else{
+        productoEnCarrito.push({ ...e, cantidad: 1 });
+      } else {
         productoExistente.cantidad++;
       }
       localStorage.setItem(
